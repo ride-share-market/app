ac8736aeba8eba1c7ceac10858d10e6ddd0d0cd6
Rudi Starcevic <ooly.me@gmail.com>
2015-07-14 14:43:51 +0800

    Add feature URL redirect with out non angular URL params, bump version

--- app/components/app/app-routes.js
+++ app/components/app/app-routes.js
@@ -34,7 +34,7 @@
         .state('about', {
           url: '/about',
           templateUrl: 'components/app/templates/app-template-about-us.html',
-          controller: function($scope, rsmConfig) {
+          controller: function ($scope, rsmConfig) {
             $scope.version = rsmConfig.version;
           },
           data: {noAuth: true}
@@ -57,6 +57,74 @@
           templateUrl: 'components/app/templates/app-template-contact.html',
           data: {noAuth: true}
         });
+
+    })
+    .run(function ($rootScope, $window, $location) {
+
+      $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState) {
+
+        if ($location.port() === 3000) {
+          return;
+        }
+
+        /**
+         * Clean up url tracking params
+         * Eg: utm_source=email&utm_medium=mobilecpc&utm_campaign=ads
+         *
+         * Note: It's best to not link with tracking codes direct to the Angular App.
+         * Better to track to a dedicated landing page for tracking campaigns/promos etc.
+         * Linking direct to angular with tracking codes will trigger another page load (below code).
+         *
+         * If the user enters on a URL like:
+         * website.com?utm_source=email&utm_medium=mobilecpc&utm_campaign=ads
+         * after angular routing it will end up like:
+         * http://local.website.com/?utm_source=email&utm_medium=mobilecpc&utm_campaign=ads#!/
+         *
+         * Subsequent clicks would end up like:
+         * website.com/?utm_source=email&utm_medium=mobilecpc&utm_campaign=ads#!/terms
+         * or if app params
+         * website.com/?utm_source=email&utm_medium=mobilecpc&utm_campaign=ads#!/terms?d=3
+         *
+         * The following code for the subsequent links after then landing page will reload the page into:
+         * website.com/#!/terms
+         * or if app params
+         * website.com/#!/terms?d=3
+         *
+         */
+
+        if (fromState.name) {
+
+          var urlParams = /com.*#/.exec($location.absUrl());
+
+          if (urlParams[0] && urlParams[0] !== 'com/#') {
+
+            var toUrl = $location.protocol() + '://' + $location.host() + '/#!' + $location.path();
+
+            var searchObject = $location.search();
+
+            if (Object.keys(searchObject).length !== 0) {
+              toUrl = toUrl + '?' + serialize(searchObject);
+            }
+
+            event.preventDefault();
+
+            $window.location = toUrl;
+          }
+        }
+
+      });
+
+      function serialize(obj, prefix) {
+        var str = [];
+        for (var p in obj) {
+          if (obj.hasOwnProperty(p)) {
+            var k = prefix ? prefix + '[' + p + ']' : p,
+              v = obj[p];
+            str.push(typeof v === 'object' ? serialize(v, k) : encodeURIComponent(k) + '=' + encodeURIComponent(v));
+          }
+        }
+        return str.join('&');
+      }
 
     });
 
--- app/components/app/app-routes_spec.js
+++ app/components/app/app-routes_spec.js
@@ -3,38 +3,107 @@
 
   describe('App Routes', function () {
 
-    beforeEach(module('app.routes'));
+    describe('State', function () {
 
-    var $state;
+      beforeEach(module('app.routes'));
 
-    beforeEach(inject(function (_$state_) {
-      $state = _$state_;
-    }));
+      var $state;
 
-    it('should return all of the state configs', function (done) {
+      beforeEach(inject(function (_$state_) {
+        $state = _$state_;
+      }));
 
-      var list = $state.get().sort(function (a, b) {
-        return (a.name > b.name) - (b.name > a.name);
+      it('should return all of the state configs', function (done) {
+
+        var list = $state.get().sort(function (a, b) {
+          return (a.name > b.name) - (b.name > a.name);
+        });
+
+        list.sort();
+
+        var names = [
+          '', // implicit root state
+          'home',
+          '404',
+          'error',
+          'about',
+          'privacy',
+          'terms',
+          'contact'
+        ].sort();
+
+        expect(list.map(function (state) {
+          return state.name;
+        })).to.eql(names);
+
+        done();
+
       });
 
-      list.sort();
+    });
 
-      var names = [
-        '', // implicit root state
-        'home',
-        '404',
-        'error',
-        'about',
-        'privacy',
-        'terms',
-        'contact'
-      ].sort();
+    describe('URL params clean up', function () {
 
-      expect(list.map(function (state) {
-        return state.name;
-      })).to.eql(names);
+      var $locationMock = {
+        protocol: function() {
+          return 'https';
+        },
+        port: function() {
+          return 80;
+        },
+        host: function() {
+          return 'website.com';
+        },
+        url: function() {
+          return '/';
+        },
+        absUrl: function () {
+          return 'http://website.com?utm_source=email&utm_medium=mobilecpc&utm_campaign=ads/#!/';
+        },
+        path: function() {
+          return '/about';
+        },
+        search: function() {
+          return {
+            key1: 'value1',
+            key2: 'value2'
+          };
+        }
+      };
 
-      done();
+      beforeEach(module('app.routes', function ($provide) {
+        $provide.value('$location', $locationMock);
+        $provide.value('$window', {});
+
+      }));
+
+      var $rootScope,
+        $state,
+        $window;
+
+      beforeEach(inject(function (_$rootScope_, _$state_, _$window_) {
+        $state = _$state_;
+        $rootScope = _$rootScope_;
+        $window = _$window_;
+      }));
+
+      it('should redirect without non Angular URL params', function () {
+
+        var toState = {
+          url: '/create'
+        };
+
+        var toParams = {};
+
+        var fromState = {
+          name: 'home'
+        };
+
+        $rootScope.$broadcast('$stateChangeStart', toState, toParams, fromState);
+
+        $window.location.should.equal('https://website.com/#!/about?key1=value1&key2=value2');
+
+      });
 
     });
 
